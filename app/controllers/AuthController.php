b159545e25dde7d90f542becddd1c7f45868ae0c
YosrZaroui <zaroui.yosr@gmail.com>
2026-05-18 00:37:21 +0100

    modifier connexion

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -1,2 +1,54 @@
 <?php
+class AuthController {
+
+    public function loginForm(): void {
+        require __DIR__ . '/../views/auth/login.php';
+    }
+public function login(): void {
+    $user = (new User)->findByEmail($_POST['email']);
+
+    if ($user && password_verify($_POST['password'], $user['password'])) {
+        $_SESSION['user_id']  = $user['id'];
+        $_SESSION['username'] = $user['username'];
+        $_SESSION['role']     = $user['role'];
+
+        if ($user['role'] === 'admin') {
+            header("Location: /SouqTN/public/dashboard/admin");
+
+        } else {
+            header("Location: /SouqTN/public/");
+        }
+        exit;
+
+    } else {
+        $error = "Email ou mot de passe incorrect";
+        require __DIR__ . '/../views/auth/login.php';
+    }
+}
+
+    public function registerForm(): void {
+        require __DIR__ . '/../views/auth/register.php';
+    }
+
+    public function register(): void {
+        $userModel = new User;
+
+        if ($userModel->emailExists($_POST['email'])) {
+            $error = "Email déjà utilisé";
+            require __DIR__ . '/../views/auth/register.php';
+            return;
+        }
+
+        $userModel->create($_POST['username'], $_POST['email'], $_POST['password']);
+header("Location: /SouqTN/public/index.php/login");
+        exit;
+    }
+
+    public function logout(): void {
+        session_destroy();
+        header("Location: /SouqTN/public/login");
+        exit;
+    }
+}
+
 
