71b80934cdc680ad01224799bd00aaa413f82c9f
YosrZaroui <zaroui.yosr@gmail.com>
2026-05-17 14:20:51 +0100

    CONNECTION BD

--- core/Database.php
+++ core/Database.php
@@ -1,2 +1,16 @@
 <?php
+class Database {
+    private static ?PDO $instance = null;
 
+    public static function getConnection(): PDO {
+        if (self::$instance === null) {
+            self::$instance = new PDO(
+                "mysql:host=localhost;dbname=souqtn;port=3306;charset=utf8",
+                "root", "",
+                [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]
+            );
+        }
+        return self::$instance;
+    }
+}
--- core/Router.php
+++ core/Router.php
@@ -1,2 +1,30 @@
 <?php
+class Router {
+    private array $routes = [];
 
+    public function get(string $path, array $handler): void {
+        $this->routes['GET'][$path] = $handler;
+    }
+
+    public function post(string $path, array $handler): void {
+        $this->routes['POST'][$path] = $handler;
+    }
+public function dispatch(): void {
+    $method = $_SERVER['REQUEST_METHOD'];
+    $uri    = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+    $uri = preg_replace('#^/SouqTN/public(?:/index\.php)?#', '', $uri);
+    $uri = ($uri === '' || $uri === null) ? '/' : $uri;
+
+    if ($method === 'POST') error_log("POST URI: [$uri] | Routes: " . implode(', ', array_keys($this->routes['POST'] ?? [])));
+
+    if (isset($this->routes[$method][$uri])) {
+        [$controller, $action] = $this->routes[$method][$uri];
+        (new $controller)->$action();
+    } else {
+        http_response_code(404);
+        echo "<pre>404\nURI reçue : [$uri]\nMéthode : $method\n\nRoutes POST dispo :\n"
+             . implode("\n", array_keys($this->routes['POST'] ?? [])) . "</pre>";
+    }
+}
+}
