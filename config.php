8c3c1300954c30594676a3ee2eb2c42f28cec008
YosrZaroui <zaroui.yosr@gmail.com>
2026-05-20 21:26:43 +0100

    modification conf

--- config.php
+++ config.php
@@ -1,2 +1,18 @@
 <?php
 
+
+$host="localhost";
+$username="root";
+$pass="";
+$dbname="souqtn";
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+try {
+    $pdo=new PDO("mysql:host=$host;dbname=$dbname;port=3306", $username, $pass);
+    
+    
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
